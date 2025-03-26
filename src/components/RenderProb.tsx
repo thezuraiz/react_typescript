8bd2fc38c3f2df3a6401efd59f5653b2e2420dd8
The Zuraiz <thezuraiz@gmail.com>
2025-03-26 15:19:03 +0500

    Solve a render Problem

---
+++ src/components/RenderProb.tsx
@@ -0,0 +1,73 @@
+import { useRef, useState } from "react";
+
+const RenderProb = () => {
+  console.log("Component Rendered");
+
+  const email = useRef<HTMLInputElement | null>(null);
+  const password = useRef<HTMLInputElement | null>(null);
+  const name = useRef<HTMLInputElement | null>(null);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+  };
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            // value={formData.name}
+            // onChange={(e) => handleChange(e)}
+            ref={name}
+            required
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            // value={formData.email}
+            // onChange={(e) => handleChange(e)}
+            ref={email}
+            required
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            // value={formData.password}
+            // onChange={(e) => handleChange(e)}
+            ref={password}
+            required
+          />
+        </div>
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+      {isSubmitted && (
+        <div>
+          <h3>Submitted Data:</h3>
+          <p>
+            <strong>Name:</strong> {name.current?.value}
+          </p>
+          <p>
+            <strong>Email:</strong> {email.current?.value}
+          </p>
+          <p>
+            <strong>Password:</strong> {password.current?.value}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RenderProb;
