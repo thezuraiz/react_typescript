18c7bc191cfb59b8c2397258cf2a497dd15b4df5
The Zuraiz <thezuraiz@gmail.com>
2025-03-27 12:59:35 +0500

    Use ShadCN Button

---
+++ src/components/shadcn_prac/ShadButtons.tsx
@@ -0,0 +1,33 @@
+import { ChevronRight, Loader2, MailOpen } from "lucide-react";
+import { Button } from "../ui/button";
+
+const ShadButtons = () => {
+  return (
+    <>
+      <h3 className="py-3 font-mono italic text-2xl text-center ">
+        Shad CN Buttons
+      </h3>
+      <div className="flex  items-center justify-between gap-2 my-5 flex-wrap p-5">
+        <Button>Click me</Button>
+        <Button variant={"outline"}>Outline</Button>
+        <Button variant={"destructive"}>Destructive</Button>
+        <Button variant={"secondary"}>secondary</Button>
+        <Button variant={"ghost"}>Ghost</Button>
+        <Button variant={"link"}>link</Button>
+
+        <Button>
+          <ChevronRight />
+        </Button>
+        <Button>
+          <MailOpen /> Login with Email
+        </Button>
+        <Button disabled>
+          <Loader2 className="animate-spin" />
+          Please wait
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default ShadButtons;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import RenderProb from "./components/RenderProb";
+import ShadButtons from "./components/shadcn_prac/ShadButtons";
+import ShadAccordion from "./components/shadcn_prac/ShadAccordion";
+import { ShadAlert } from "./components/shadcn_prac/ShadAlert";
+
+// import RenderProb from "./components/RenderProb";
+
 // import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
-    <RenderProb />
+    <div className="bg-slate-600 flex justify-center">
+      <div className="bg-slate-300  h-screen max-w-lg p-5 space-y-4">
+        {/* <App /> */}
+        {/* <RenderProb /> */}
+        <ShadButtons />
+        <ShadAccordion />
+        <ShadAlert />
+      </div>
+    </div>
   </StrictMode>
 );
