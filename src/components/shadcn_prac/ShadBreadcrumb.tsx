7095bb9cb2ea6508985c1df032bd805c2efeeebd
The Zuraiz <thezuraiz@gmail.com>
2025-03-27 16:02:11 +0500

    Use ShadCn BreadCrumb

---
+++ src/components/shadcn_prac/ShadBreadcrumb.tsx
@@ -0,0 +1,35 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+const ShadBreadcrumb = () => {
+  return (
+    <div>
+      <h3 className="py-3 italic text-xl font-medium text-center">
+        Shad CN BreadCrumb
+      </h3>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+};
+
+export default ShadBreadcrumb;
