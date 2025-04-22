df6f0e78874ce451c39238bcf8f4b6c0b68f644b
The Zuraiz <thezuraiz@gmail.com>
2025-04-22 16:13:00 +0500

    Recommended way

--- src/dashbboard/pages/form-handling/form.tsx
+++ src/dashbboard/pages/form-handling/form.tsx
@@ -2,25 +2,17 @@ import { useForm } from "react-hook-form";
 const FormHandling = () => {
   const form = useForm();
   let { register } = form;
-  const { name, ref, onChange, onBlur } = register("username"); // Old Way
+  //   const { name, ref, onChange, onBlur } = register("username"); // Old Way
   return (
     <div className="bg-black text-white h-screen">
       <form className="flex flex-col w-1/3 p-5 ">
         <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name={name}
-          ref={ref}
-          onChange={onChange}
-          onBlur={onBlur}
-        />
-
+        <input type="text" id="username" {...register("username")} />
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
-
+        <input type="email" id="email" {...register("email")} />
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
-
+        <input type="text" id="channel" {...register("channel")} />{" "}
+        {/* recommeded way */}
         <button className="border-2 border-gray-100 rounded-md p-2 my-4">
           Submit
         </button>
